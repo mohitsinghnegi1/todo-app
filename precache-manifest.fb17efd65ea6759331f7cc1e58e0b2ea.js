eef33702168f0b4a1a42940534d1c365addec0d9
mohitsinghnegi1 <37727115+mohitsinghnegi1@users.noreply.github.com>
2020-08-10 02:48:34 +0530

    Updates

---
+++ precache-manifest.fb17efd65ea6759331f7cc1e58e0b2ea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "500c51341d542c6a9d84402bebebb033",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "36d76d34bbbf029e1e9f",
+    "url": "/todo-app/static/css/main.2af6de3a.chunk.css"
+  },
+  {
+    "revision": "8dbfbabe0624268e4b67",
+    "url": "/todo-app/static/js/2.8fdf4d44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.8fdf4d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d76d34bbbf029e1e9f",
+    "url": "/todo-app/static/js/main.b854d063.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  },
+  {
+    "revision": "262bc2ee2d503a65cda83c0a8cbf7af2",
+    "url": "/todo-app/static/media/img1.262bc2ee.jpg"
+  },
+  {
+    "revision": "2762f3fe4bb4cef0bc94270fd9d33aa5",
+    "url": "/todo-app/static/media/img2.2762f3fe.jpeg"
+  },
+  {
+    "revision": "50278dadada4c1b75d6603cc493fe9a2",
+    "url": "/todo-app/static/media/img3.50278dad.jpeg"
+  },
+  {
+    "revision": "60abd470f9d3b0a8be2b0bbab99a9100",
+    "url": "/todo-app/static/media/user-tick.60abd470.png"
+  }
+]);
